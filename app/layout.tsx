f4ffacca2a71a8b9580fbcab3b731064806a9336
justynalodzinska <justyna.lodzinska@smartive.app>
2025-02-08 17:06:05 +0100

    colors and fonts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,26 @@
 import "./globals.css";
-
+import { cn } from "@/utils/lib/tailwind";
+import { Inter } from "next/font/google";
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['300', '400', '500', '700']
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
+
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={cn(
+          'min-h-screen bg-background text-foreground font--poppins antialiased',
+          inter.variable
+        )}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
   content: [
@@ -8,9 +9,15 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)", ...fontFamily.sans]
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        green: "var(--green)",
+        muted: "var(--muted)",
+        accent: "var(--accent)",
       },
     },
   },
